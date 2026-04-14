17567aa539192a66e61a4abf32c23a04f01a8bcd
soniiix <quent.couzinet@gmail.com>
2026-04-14 11:08:57 +0200

    fix: change api url

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -51,7 +51,7 @@ export default function LoginModal({ isOpen, onClose, onOpenRegister, onLoginSuc
         setIsLoading(true);
 
         try {
-            const res = await fetch("http://78.138.58.74:8080/api/login", {
+            const res = await fetch("https://vps115222.serveur-vps.net/api/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
